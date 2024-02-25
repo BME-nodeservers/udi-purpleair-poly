4a7a8d51a23529bcc33d9bcd06fbafa25d1e00a0
Bob Paauwe <bpaauwe@yahoo.com>
2024-02-25 08:58:09 -0800

    Version 2.1.3

--- purple.py
+++ purple.py
@@ -9,12 +9,14 @@ import sys
 import time
 from nodes import purpleair
 
+VERSION = '2.1.3'
 LOGGER = udi_interface.LOGGER
 
 if __name__ == "__main__":
     try:
         polyglot = udi_interface.Interface([purpleair.Controller])
-        polyglot.start('2.1.2')
+        LOGGER.info('Starting PurpleAir Plug-in version {}'.format(VERSION))
+        polyglot.start(VERSION)
         control = purpleair.Controller(polyglot, "controller", "controller", "Purple Air AQI")
         polyglot.runForever()
     except (KeyboardInterrupt, SystemExit):
